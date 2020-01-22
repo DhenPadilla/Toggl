e89aebf287f3c0aa2de761449aa3c4658696e505
Dhen E Padilla <dhenepadilla@gmail.com>
2020-01-22 18:30:51 +0000

    added GraphQL integration

--- api/app.js
+++ api/app.js
@@ -1,19 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const testRouter  = require('./routes/testAPI');
 
-var app = express();
+
+const app = express();
+
+// GraphQL Schema
+const graphqlSchema = require('./schema/schema')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,6 +30,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/testApi', testRouter);
+
+app.use('/graphql', graphqlHTTP({
+  schema: graphqlSchema,
+  graphiql: true,
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
